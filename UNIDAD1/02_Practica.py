d5b9ac85b079445cea97fba69936ece1efdf5dbe
JocelynRdzPon <jocerdzp26@gmail.com>
2023-02-20 16:42:15 -0600

    Practicas 1-7

--- UNIDAD1/02_Practica.py
+++ UNIDAD1/02_Practica.py
@@ -5,10 +5,10 @@ posiciones múltiplos de 3, y muestre por pantalla la lista resultante.
 """
 ##ABECEDARIO
 abecedario =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-listanueva = []
-print("ABECEDARIO COMPLETO", abecedario)
-# ##########################
-for letra  in range(len(abecedario),1,-1):
+longitud = len(abecedario)
+print("ABECEDARIO COMPLETO \n", abecedario, "\n")
+
+for letra  in range(longitud,1,-1):
     if letra % 3 == 0:
         abecedario.pop(letra - 1)
-print(abecedario)
+print("ABECEDARIO SIN MULTIPLOS DE 3 \n",abecedario,"\n")
